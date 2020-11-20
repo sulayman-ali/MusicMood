12b03e08d49eee277dbda693cdc11b3f33c2f4d8
sulaymanumich <sulayman@umich.edu>
2020-11-20 12:20:20 -0500

    multithreading enabled

--- pullData.py
+++ pullData.py
@@ -4,6 +4,9 @@ import requests
 import json
 import urllib
 import numpy as np
+import pandas as pd
+import pickle
+import multiprocessing
 
 CLIENT_ID = open("secrets.txt").read().splitlines()[0]
 CLIENT_SECRET = open("secrets.txt").read().splitlines()[1]
@@ -40,11 +43,11 @@ def getsong_details(uri,headers):
 	results = {}
 	try:
 		r = requests.get(url, headers = headers).text
+		#print(r)
 		j = json.loads(r)
 		# results.append((
 		# 	j['danceability'],j['energy'],j['key'],j['mode'],j['acousticness'],j['instrumentalness'],
 		# 	j['tempo'],j["valence"],j["speechiness"],j["loudness"]))
-
 		results["danceability"] = j['danceability']
 		results["energy"] = j['energy']
 		results["key"] = j['key']
@@ -57,8 +60,44 @@ def getsong_details(uri,headers):
 		results["loudness"] = j['loudness']
 
 	except:
-		print("error")
+		return "error"
+
 	return results
+
+def fetch_songs_from_playlist(session,query,playlist):
+# def fetch_songs_from_playlist(url,session):
+	access_token = authenticate(CLIENT_ID,CLIENT_SECRET)
+	headers = {'Authorization': 'Bearer {token}'.format(token=access_token)}
+	url = "https://api.spotify.com/v1/playlists/{p}/tracks".format(p = playlist)
+	# r = requests.get(url, headers = headers).text
+	r = session.get(url, headers = headers).text
+	#print(r)
+	result = []
+	try:
+		for s in range(len(json.loads(r)["items"])):
+				try:
+					rj = json.loads(r)
+					uri = rj["items"][s]["track"]["uri"].split(":")[2]
+					title = rj["items"][s]["track"]["name"]
+					artist = rj["items"][s]["track"]["album"]["artists"][0]["name"]
+					audio_features = getsong_details(uri,headers)
+					audio_features["title"] = title
+					audio_features["artist"] = artist
+					audio_features["class"] = query
+					audio_features["playlist_id"] = playlist
+					result.append(audio_features)
+				except:
+					continue
+	except:
+		return None
+
+	return json.dumps(result)
+
+def multiprocess_queries(ids,query):
+	pool = multiprocessing.Pool()
+	with requests.Session() as session:
+		result = pool.starmap(fetch_songs_from_playlist,[(session,query,playlist) for playlist in ids])
+	return result 
 
 def query_playlists(query):
 	'''
@@ -74,28 +113,37 @@ def query_playlists(query):
 	for n in range(len(json.loads(r)["playlists"]['items'])):
 	    playlistIDs.append((json.loads(r)["playlists"]['items'][n]["id"]))
 
-	result = []
-	#for each playlist in search result, get every song and audio features
-	for playlist in playlistIDs:
-		url = "https://api.spotify.com/v1/playlists/{p}/tracks".format(p = playlist)
-		r = requests.get(url, headers = headers).text
-		for s in range(len(json.loads(r)["items"])):
-			try:
-				rj = json.loads(r)
-				uri = rj["items"][s]["track"]["uri"].split(":")[2]
-				title = rj["items"][s]["track"]["name"]
-				artist = rj["items"][s]["track"]["album"]["artists"][0]["name"]
-				audio_features = getsong_details(uri,headers)
-				audio_features["title"] = title
-				audio_features["artist"] = artist
-				audio_features["class"] = query
-				audio_features["playlist_id"] = playlist
-				result.append(audio_features)
-			except:
-				continue
+	urls = ["https://api.spotify.com/v1/playlists/{p}/tracks".format(p = playlist) for playlist in playlistIDs]
+	result = multiprocess_queries(playlistIDs,query)
+	# #result = []
+	# #for each playlist in search result, get every song and audio features
+	# for playlist in playlistIDs:
+	# 	url = "https://api.spotify.com/v1/playlists/{p}/tracks".format(p = playlist)
+	# 	r = requests.get(url, headers = headers).text
+	# 	for s in range(len(json.loads(r)["items"])):
+	# 		try:
+	# 			rj = json.loads(r)
+	# 			uri = rj["items"][s]["track"]["uri"].split(":")[2]
+	# 			title = rj["items"][s]["track"]["name"]
+	# 			artist = rj["items"][s]["track"]["album"]["artists"][0]["name"]
+	# 			audio_features = getsong_details(uri,headers)
+	# 			audio_features["title"] = title
+	# 			audio_features["artist"] = artist
+	# 			audio_features["class"] = query
+	# 			audio_features["playlist_id"] = playlist
+	# 			result.append(audio_features)
+	# 		except:
+	# 			continue
 
-	return json.dumps(result)
-
+	#return json.dumps(result)
+	return result
 
 if __name__ == "__main__":
-	print(query_playlists("hype"))
+	#df = pd.read_csv("data/emotion_queries.csv")
+	#results = []
+	#for q in df["query"]:
+	#	results.append(query_playlists(q))
+	#pickle.dump(results, open("search_results.pkl", 'wb'))
+	print(query_playlists("tender"))
+	#pool = multiprocessing.Pool()
+	#print(pool.map(cube,[1,2,3]))
